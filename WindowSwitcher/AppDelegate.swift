c97a7f40649982c2aee7c6d380b374fb96770479
Andrew <andrewtawf@gmail.com>
2019-03-23 21:11:16 +0200

    Initial Commit

---
+++ WindowSwitcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WindowSwitcher
+//
+//  Created by Andrew Magdy on 3/23/19.
+//  Copyright © 2019 Andrew Magdy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
